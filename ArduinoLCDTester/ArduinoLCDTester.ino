c87a4a2bdf8283a58926b1e1b8c916080c9576ff
miguelvazqueznaya <miguelvnaya@gmail.com>
2024-10-08 18:52:06 +0100

    finished working LCD

--- ArduinoLCDTester/ArduinoLCDTester.ino
+++ ArduinoLCDTester/ArduinoLCDTester.ino
@@ -51,7 +51,7 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Hello Julieta");
+  lcd.print("Hello Everyone");
 }
 
 
@@ -61,12 +61,12 @@ void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 0);
-  lcd.print("I love you     ");
+  lcd.print("Make sure to");
   lcd.setCursor(0, 1);
-  lcd.print("My pretty girl");
+  lcd.print("Comment code");
   // print the number of seconds since reset:
   //lcd.print(millis() / 1000); 
   delay(3000);
   lcd.setCursor(0, 0);
-  lcd.print("Youre amazing");
+  lcd.print("Well done on ur");
 }
